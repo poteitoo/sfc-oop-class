9404d5ff66ba429e430198c6f17a3a98189f0ff8
poteitoo <1105tigery@gmail.com>
2023-10-06 06:14:24 +0000

    add chapter01

---
+++ chaper-01/src/main/java/com/sfcclass/app/App.java
@@ -0,0 +1,13 @@
+package com.sfcclass.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
